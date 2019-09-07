c9acd1bf7d685d010fa1838b924464adef1e5b08
Ram <ramaisgod@gmail.com>
2019-09-07 19:26:31 +0530

    s9

--- task_tracker/settings.py
+++ task_tracker/settings.py
@@ -129,7 +129,7 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 # EMAIL_HOST = 'smtp.gmail.com'
